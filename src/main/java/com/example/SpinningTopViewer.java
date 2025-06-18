2e7b5f88a662f7b434d09de12121134e3a929d66
Marcus Hennig <marcus@planqc.eu>
2025-06-18 16:52:07 +0200

    Fixed OpenGL

--- src/main/java/com/example/SpinningTopViewer.java
+++ src/main/java/com/example/SpinningTopViewer.java
@@ -6,17 +6,14 @@ import static org.lwjgl.opengl.GL11.*;
 
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
-import org.lwjgl.util.glu.Cylinder;
-import org.lwjgl.util.glu.GLU;
 
 public class SpinningTopViewer {
+    
     private SpinningTopSimulation sim = new SpinningTopSimulation();
     private double time = 0;
     private long window;
     private final int width = 800;
     private final int height = 600;
-    private final Cylinder cone = new Cylinder();
-    private final GLU glu = new GLU();
 
     public static void main(String[] args) {
         new SpinningTopViewer().run();
@@ -46,7 +43,7 @@ public class SpinningTopViewer {
         glEnable(GL_DEPTH_TEST);
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glu.gluPerspective(45.0f, (float) width / height, 1.0f, 500.0f);
+        perspective(45.0f, (float) width / height, 1.0f, 500.0f);
         glMatrixMode(GL_MODELVIEW);
     }
 
@@ -55,7 +52,7 @@ public class SpinningTopViewer {
             glfwPollEvents();
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             glLoadIdentity();
-            glu.gluLookAt(0, 0, 100, 0, 0, 0, 0, 1, 0);
+            lookAt(0, 0, 100, 0, 0, 0, 0, 1, 0);
 
             glRotated(sim.getParameter().phi * 180 / Math.PI, 0, 1, 0);
             glRotated(sim.getParameter().theta * 180 / Math.PI, 1, 0, 0);
@@ -71,6 +68,51 @@ public class SpinningTopViewer {
 
     private void drawTop() {
         glColor3f(0.8f, 0.2f, 0.2f);
-        cone.draw(5f, 0f, 20f, 20, 20);
+    }
+
+    private void perspective(float fovy, float aspect, float zNear, float zFar) {
+        float fH = (float) Math.tan(Math.toRadians(fovy / 2)) * zNear;
+        float fW = fH * aspect;
+        glFrustum(-fW, fW, -fH, fH, zNear, zFar);
+    }
+
+    private void lookAt(
+            double eyeX, double eyeY, double eyeZ,
+            double centerX, double centerY, double centerZ,
+            double upX, double upY, double upZ) {
+        double[] forward = {
+            centerX - eyeX,
+            centerY - eyeY,
+            centerZ - eyeZ
+        };
+        double fLen = Math.sqrt(forward[0]*forward[0] + forward[1]*forward[1] + forward[2]*forward[2]);
+        forward[0] /= fLen; forward[1] /= fLen; forward[2] /= fLen;
+
+        double[] up = { upX, upY, upZ };
+        double upLen = Math.sqrt(up[0]*up[0] + up[1]*up[1] + up[2]*up[2]);
+        up[0] /= upLen; up[1] /= upLen; up[2] /= upLen;
+
+        double[] side = {
+            forward[1]*up[2] - forward[2]*up[1],
+            forward[2]*up[0] - forward[0]*up[2],
+            forward[0]*up[1] - forward[1]*up[0]
+        };
+        double sLen = Math.sqrt(side[0]*side[0] + side[1]*side[1] + side[2]*side[2]);
+        side[0] /= sLen; side[1] /= sLen; side[2] /= sLen;
+
+        double[] up2 = {
+            side[1]*forward[2] - side[2]*forward[1],
+            side[2]*forward[0] - side[0]*forward[2],
+            side[0]*forward[1] - side[1]*forward[0]
+        };
+
+        float[] m = new float[] {
+            (float)side[0],   (float)up2[0],   (float)-forward[0],   0,
+            (float)side[1],   (float)up2[1],   (float)-forward[1],   0,
+            (float)side[2],   (float)up2[2],   (float)-forward[2],   0,
+            0,                0,               0,                    1
+        };
+        glMultMatrixf(m);
+        glTranslated(-eyeX, -eyeY, -eyeZ);
     }
 }
