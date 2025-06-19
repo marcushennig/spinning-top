724b1a5313c98722db196c0977a956c78f7ba9aa
Marcus Hennig <marcus.hennig@icloud.com>
2025-06-19 18:02:36 +0200

    Add trajectory and arrow visualization

--- src/main/java/com/example/SpinningTopViewer.java
+++ src/main/java/com/example/SpinningTopViewer.java
@@ -9,6 +9,10 @@ import com.jme3.font.BitmapText;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.debug.Arrow;
+import com.jme3.scene.Mesh;
+import com.jme3.scene.VertexBuffer;
+import com.jme3.util.BufferUtils;
 import com.jme3.system.AppSettings;
 
 /**
@@ -20,6 +24,9 @@ public class SpinningTopViewer extends SimpleApplication {
     private double time = 0.0;
     private Geometry topGeom;
     private BitmapText hudText;
+    private Geometry axisArrowGeom;
+    private Geometry trajectoryGeom;
+    private final java.util.List<Vector3f> trajectory = new java.util.ArrayList<>();
 
     public static void main(String[] args) {
         var app = new SpinningTopViewer();
@@ -70,6 +77,22 @@ public class SpinningTopViewer extends SimpleApplication {
         hudText.setColor(ColorRGBA.Black);
         hudText.setLocalTranslation(10f, cam.getHeight() - 10f, 0);
         guiNode.attachChild(hudText);
+
+        // Red arrow indicating the current (x,y,z) position of the centre of mass
+        var arrow = new Arrow(Vector3f.UNIT_Z.mult((float) simulation.getConstants().l));
+        arrow.setLineWidth(2);
+        axisArrowGeom = new Geometry("comArrow", arrow);
+        var redMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        redMat.setColor("Color", ColorRGBA.Red);
+        axisArrowGeom.setMaterial(redMat);
+        rootNode.attachChild(axisArrowGeom);
+
+        // Blue polyline for the recent trajectory
+        trajectoryGeom = new Geometry("trajectory", new Mesh());
+        var blueMat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+        blueMat.setColor("Color", ColorRGBA.Blue);
+        trajectoryGeom.setMaterial(blueMat);
+        rootNode.attachChild(trajectoryGeom);
     }
     /**
      * Resets the simulation to initial conditions.
@@ -91,9 +114,43 @@ public class SpinningTopViewer extends SimpleApplication {
                 .mult(new Quaternion().fromAngleAxis((float) s.psi, Vector3f.UNIT_Z));
         topGeom.setLocalRotation(rot);
 
+        // Compute centre-of-mass position from Euler angles
+        var comPos = rot.mult(new Vector3f(0f, 0f, (float) simulation.getConstants().l));
+        ((Arrow) axisArrowGeom.getMesh()).setArrowExtent(comPos);
+        axisArrowGeom.updateModelBound();
+
+        updateTrajectory(comPos);
+        updateTrajectoryGeometry();
+
         hudText.setText(String.format(
                 "Time: %.3f\nEnergy: %.3f\ntheta: %.3f\nphi: %.3f\npsi: %.3f",
                 time, s.energy, s.theta, s.phi, s.psi));
+    }
+
+    private void updateTrajectory(Vector3f pos) {
+        trajectory.add(pos.clone());
+        if (trajectory.size() > 100) {
+            trajectory.remove(0);
+        }
+    }
+
+    private void updateTrajectoryGeometry() {
+        var mesh = new Mesh();
+        mesh.setMode(Mesh.Mode.LineStrip);
+        var pts = trajectory.toArray(Vector3f[]::new);
+        if (pts.length < 2) {
+            trajectoryGeom.setMesh(mesh);
+            return;
+        }
+        int[] indices = new int[pts.length];
+        for (int i = 0; i < pts.length; i++) {
+            indices[i] = i;
+        }
+        mesh.setBuffer(VertexBuffer.Type.Position, 3, BufferUtils.createFloatBuffer(pts));
+        mesh.setBuffer(VertexBuffer.Type.Index, 1, BufferUtils.createIntBuffer(indices));
+        mesh.updateBound();
+        mesh.updateCounts();
+        trajectoryGeom.setMesh(mesh);
     }
 
 }
